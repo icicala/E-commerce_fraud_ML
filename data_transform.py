17e37c192411a01bc50a941dece2ee173da87011
icicala <icicala.ubb@gmail.com>
2024-01-20 22:15:18 +0100

    FRAUD-25 Univariate analysis Descriptive Statistics

---
+++ data_transform.py
@@ -0,0 +1,53 @@
+# map ip address from Fraud_Data.csv to country name from IpAddress_to_Country.csv
+# save the result to EFraud_Data_Country.csv
+import os
+import pandas as pd
+from intervaltree import IntervalTree
+
+
+# read data_fraud from csv file
+def read_data_fraud(filename):
+    # Get the path of the file
+    filename = os.path.join(os.path.dirname(__file__), 'data', filename)
+    data = pd.read_csv(filename, parse_dates=['signup_time', 'purchase_time'],
+                       converters={'ip_address': lambda x: preprocess_ip_to_int(x)})
+    return data.copy()
+# Read data_ip from csv file
+def load_ip(filename):
+
+    filename = os.path.join(os.path.dirname(__file__), 'data', filename)
+    ip = pd.read_csv(filename, sep=';')
+    return ip.copy()
+# replace ip address with country name
+def replace_Ip_country(data_fraud, data_ip):
+    '''
+    Ip address to country name transformation logarithm time complexity
+    :param data_fraud: Fraud_Data.csv
+    :param data_ip: IpAddress_to_Country.csv
+    :return: transformed data_fraud
+    '''
+    search_tree = IntervalTree()
+    for row in data_ip.itertuples(index=False):
+        search_tree[row.lower_bound_ip_address:row.upper_bound_ip_address] = row.country
+
+    def ip_lookup(ip):
+        result = search_tree[ip]
+        return result.pop().data if result else 'Not Found'
+
+    data_fraud['country'] = data_fraud['ip_address'].apply(ip_lookup)
+
+    return data_fraud
+# convert ip address to int
+def preprocess_ip_to_int(ip):
+    return int(ip.split('.')[0])
+# save data to csv file
+def save_data(data, filename):
+    filename = os.path.join(os.path.dirname(__file__), 'data', filename)
+    data.to_csv(filename, index=False)
+
+if __name__ == '__main__':
+    data_fraud = read_data_fraud('Fraud_Data.csv')
+    data_ip = load_ip('IpAddress_to_Country.csv')
+    data_fraud = replace_Ip_country(data_fraud, data_ip)
+    save_data(data_fraud, 'EFraud_Data_Country.csv')
+
---
+++ univariate_descriptive_statistics_report.py
@@ -0,0 +1,341 @@
+import os
+import sys
+
+import pandas as pd
+
+
+# Save the text output to a file
+def save_to_file(filename, text):
+    with open(filename, 'a') as file:
+        file.write(text)
+
+
+# Read data from csv files
+def read_data(filename, parse_dates=None, delimiter=','):
+    """
+    Read the fraud data from csv file
+    :param filename: csv file
+    :return: dataframe
+    """
+    # Get the path of the file
+    filename = os.path.join(os.path.dirname(__file__), 'data', filename)
+    # Read the data from csv file
+    data = pd.read_csv(filename, parse_dates=parse_dates, delimiter=delimiter)
+    return data.copy()
+
+
+# Exploring data frames
+def explore_data_frames(data):
+    """
+    Read the 4 first rows of the dataframe
+    :param data: dataframe
+    :return: None
+    """
+    pd.set_option('display.max_columns', None)
+    pd.set_option('expand_frame_repr', False)
+    text_output = '##############################################\n'
+    text_output += '############ Exploring data frames ###########\n'
+    text_output += '##############################################\n'
+    text_output += str(data.head()) + '\n'
+    save_to_file('report.txt', text_output)
+
+
+# Identifying missing values
+def missing_values(data):
+    """
+    Get the number of missing values
+    :param data: data frame
+    :return: None
+    """
+    text_output = '##############################################\n'
+    text_output += '############# Missing values #################\n'
+    text_output += '##############################################\n'
+    text_output += str(data.isnull().sum()) + '\n'
+    save_to_file('report.txt', text_output)
+
+
+# Check the data if has null values for each column
+def check_null_values(data):
+    """
+    Check if the data has null values for each column
+    :param data: data frame
+    :return: None
+    """
+    text_output = '##############################################\n'
+    text_output += '########### Check null values ################\n'
+    text_output += '##############################################\n'
+    text_output += str(data.isnull().any()) + '\n'
+    save_to_file('report.txt', text_output)
+
+
+# datatframe data types
+def data_types(data):
+    """
+    Get the data types of each column
+    :param data: data frame
+    :return: None
+    """
+    text_output = '##############################################\n'
+    text_output += '############ Data types ######################\n'
+    text_output += '##############################################\n'
+    # Map pandas data types to Python types
+    python_types = {
+        'int64': 'int',
+        'float64': 'float',
+        'object': 'str',
+        'bool': 'bool',
+        'datetime64[ns]': 'datetime',
+    }
+
+    # Use apply to map data types for each column
+    data_types_mapped = data.apply(lambda x: python_types.get(str(x.dtypes), 'Unknown')).rename('Data types')
+    dtypes_df = pd.DataFrame(data_types_mapped)
+
+    text_output += str(dtypes_df) + '\n'
+
+    save_to_file('report.txt', text_output)
+
+
+# Data Frames attributes
+def data_metadata(data):
+    """
+    Get the data metadata
+    :param data: data frame
+    :return: None
+    """
+    text_output = '##############################################\n'
+    text_output += '################ Data metadata ###############\n'
+    text_output += '##############################################\n'
+    # Get the data frames attributes
+    metadatas = ['index', 'shape', 'size', 'ndim', 'empty']
+    for metadata in metadatas:
+        text_output += 'Data frame attribute: ' + metadata + '\n'
+        text_output += str(getattr(data, metadata)) + '\n'
+    save_to_file('report.txt', text_output)
+
+
+# Descriptive Statistics for numeric columns
+def descriptive_statistics_numerical(data):
+    """
+    Get the descriptive statistics for numeric columns
+    :param data: data frame
+    :return: None
+    """
+    pd.options.display.float_format = '{:,.2f}'.format
+    # Remove from data datatime columns
+    data = data.select_dtypes(include=['int64', 'float64'])
+    text_output = '##############################################\n'
+    text_output += '### Descriptive statistics Numerical data ####\n'
+    text_output += '##############################################\n'
+    text_output += str(data.describe()) + '\n'
+    save_to_file('report.txt', text_output)
+
+
+# Check if user_id is is assigned sequentially
+def check_user_id(data):
+    data_sorted = data.sort_values(by=['user_id'])
+    is_sequential = data_sorted['user_id'].is_monotonic_increasing
+    text_output = '##############################################\n'
+    text_output += '########### Check user_id sequence ###########\n'
+    text_output += '##############################################\n'
+    text_output += 'Is user_id sequential: ' + str(is_sequential) + '\n'
+    save_to_file('report.txt', text_output)
+
+
+# Get the descriptive statistics for categorical columns
+def descriptive_statistics_categorical(data):
+    """
+    Get the descriptive statistics for categorical columns
+    :param data: data frame
+    :return: None
+    """
+    text_output = '##############################################\n'
+    text_output += '## Descriptive statistics categorical data ###\n'
+    text_output += '##############################################\n'
+    text_output += str(data.describe(include='object')) + '\n'
+    save_to_file('report.txt', text_output)
+
+
+# Check all date from datetime columns have format='%Y-%m-%d %H:%M:%S'
+def check_datetime_format(data):
+    """
+    Check all date from datetime columns have format='%Y-%m-%d %H:%M:%S'
+    :param data: data frame
+    :return: None
+    """
+    text_output = '##############################################\n'
+    text_output += '########### Check datetime format ############\n'
+    text_output += '##############################################\n'
+    datetime_columns = ['signup_time', 'purchase_time']
+    for datetime_column in datetime_columns:
+        try:
+            pd.to_datetime(data[datetime_column], format='%Y-%m-%d %H:%M:%S')
+            text_output += 'All dates from ' + datetime_column + ' have format=%Y-%m-%d %H:%M:%S\n'
+        except ValueError:
+            text_output += 'Not all dates from ' + datetime_column + ' have format=%Y-%m-%d %H:%M:%S\n'
+    save_to_file('report.txt', text_output)
+
+
+# Date time analysis
+def descriptive_statistics_datetime(data):
+    """
+    Get the descriptive statistics for datetime columns
+    :param data: data frame
+    :return: None
+    """
+    text_output = '##############################################\n'
+    text_output += '## Descriptive statistics datetime data #####\n'
+    text_output += '##############################################\n'
+    # Get min and max of datetime columns
+    datetime_columns = ['signup_time', 'purchase_time']
+    for datetime_column in datetime_columns:
+        text_output += 'Earliest date ' + datetime_column + ': ' + str(data[datetime_column].min()) + '\n'
+        text_output += 'Latest Date ' + datetime_column + ': ' + str(data[datetime_column].max()) + '\n'
+    # Get the month with the most transactions
+    data['month'] = data['purchase_time'].dt.month
+    text_output += 'Month with the most transactions: ' + str(data['month'].mode()[0]) + '\n'
+    # Get the day of the week with the most transactions
+    data['day'] = data['purchase_time'].dt.day
+    text_output += 'Day with the most transactions: ' + str(data['day'].mode()[0]) + '\n'
+    # Get the hour with the most transactions
+    data['hour'] = data['purchase_time'].dt.hour
+    text_output += 'Hour with the most transactions: ' + str(data['hour'].mode()[0]) + '\n'
+    save_to_file('report.txt', text_output)
+
+
+# Check IpAddress_to_Country.csv data type and convert for Pandas to Python types
+def check_ip_address(data):
+    """
+    Check IpAddress_to_Country.csv data type and convert for Pandas to Python types
+    :param data: data frame
+    :return: None
+    """
+    text_output = '##############################################\n'
+    text_output += '########### Check Ip Address data type ######\n'
+    text_output += '##############################################\n'
+    # Map pandas data types to Python types
+    python_types = {
+        'int64': 'int',
+        'float64': 'float',
+        'object': 'str',
+        'bool': 'bool',
+        'datetime64[ns]': 'datetime',
+    }
+    # Use apply to map data types for each column
+    data_types_mapped = data.apply(lambda x: python_types.get(str(x.dtypes), 'Unknown')).rename('Data types')
+    dtypes_df = pd.DataFrame(data_types_mapped)
+    text_output += str(dtypes_df) + '\n'
+    save_to_file('report.txt', text_output)
+# Check missing values
+def check_missing_values(data):
+    """
+    Get the number of missing values
+    :param data: data frame
+    :return: None
+    """
+    text_output = '##############################################\n'
+    text_output += '############# Missing values #################\n'
+    text_output += '##############################################\n'
+    text_output += str(data.isnull().sum()) + '\n'
+    save_to_file('report.txt', text_output)
+# Descriptive Statistics for numerical columns IpAddress_to_Country.csv
+def descriptive_statistics_numerical_ip_address(data):
+    """
+    Get the descriptive statistics for numeric columns
+    :param data: data frame
+    :return: None
+    """
+    pd.options.display.float_format = '{:,.2f}'.format
+    # Remove from data datatime columns
+    data = data.select_dtypes(include=['int64'])
+    text_output = '##############################################\n'
+    text_output += '# Descriptive statistics Ip address columns #\n'
+    text_output += '##############################################\n'
+    text_output += str(data.describe()) + '\n'
+    save_to_file('report.txt', text_output)
+# Descriptive Statistics for categorical columns IpAddress_to_Country.csv
+def descriptive_statistics_categorical_ip_address(data):
+    """
+    Get the descriptive statistics for categorical columns
+    :param data: data frame
+    :return: None
+    """
+    text_output = '##############################################\n'
+    text_output += '## Descriptive statistics Ip-Country data ###\n'
+    text_output += '##############################################\n'
+    text_output += str(data.describe(include='object')) + '\n'
+    save_to_file('report.txt', text_output)
+
+# Descriptive Statistics for country column
+def descriptive_statistics_country(data):
+    """
+    Get the descriptive statistics for categorical columns
+    :param data: data frame
+    :return: None
+    """
+    text_output = '##############################################\n'
+    text_output += '## Descriptive statistics country data ###\n'
+    text_output += '##############################################\n'
+    text_output += str(data['country'].describe()) + '\n'
+    save_to_file('report.txt', text_output)
+
+# Find the total amount from country column 'Not Found' values
+def find_not_found_country(data):
+    """
+    Find the total amount from country column 'Not Found' values
+    :param data: data frame
+    :return: None
+    """
+    text_output = '##############################################\n'
+    text_output += '## Total amount country Not Found ##########\n'
+    text_output += '##############################################\n'
+    text_output += str(data['country'].value_counts()['Not Found']) + '\n'
+    save_to_file('report.txt', text_output)
+
+# python init main
+if __name__ == '__main__':
+    # read fraud data
+    data_fraud = read_data('Fraud_Data.csv', parse_dates=['signup_time', 'purchase_time'])
+    data_ip = read_data('IpAddress_to_Country.csv', delimiter=';')
+######## Fraud_Data.csv ###########
+    # Exploring data frames
+    explore_data_frames(data_fraud)
+    # Missing values
+    missing_values(data_fraud)
+    # Check null values
+    check_null_values(data_fraud)
+    # Datatframe data types
+    data_types(data_fraud)
+    # Data metadata
+    data_metadata(data_fraud)
+    # Descriptive statistics numerical
+    descriptive_statistics_numerical(data_fraud)
+    # Check user_id sequence
+    check_user_id(data_fraud)
+    # Descriptive statistics categorical
+    descriptive_statistics_categorical(data_fraud)
+    # Check datetime format
+    check_datetime_format(data_fraud)
+    # Descriptive statistics datetime
+    descriptive_statistics_datetime(data_fraud)
+######## IpAddress_to_Country.csv ###########
+    # Check Ip Address data type
+    check_ip_address(data_ip)
+    # Check missing values
+    check_missing_values(data_ip)
+    # Check null values
+    check_null_values(data_ip)
+    # Descriptive statistics numerical
+    descriptive_statistics_numerical_ip_address(data_ip)
+    # Descriptive statistics categorical
+    descriptive_statistics_categorical_ip_address(data_ip)
+############# Data Transform ################
+    # load transformed data
+    data_transformed = read_data('EFraud_Data_Country.csv', parse_dates=['signup_time', 'purchase_time'])
+    # Descriptive statistics country
+    descriptive_statistics_country(data_transformed)
+    # Find the total amount from country column 'Not Found' values
+    find_not_found_country(data_transformed)
+
+
+
