c7cc18c55dbe4ee302222eeaef0c302405592b60
icicala <icicala.ubb@gmail.com>
2024-02-28 00:06:05 +0100

    FRAUD-26 Setup feature engineering done

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 
 
+
 # load EFraud_Dataset with country
 # Read data from csv files
 def read_data(filename, delimiter=','):
@@ -65,8 +66,96 @@ def tukey_method(data, column):
     text_output += str(outliers) + '\n'
     save_to_file('report.txt', text_output)
 
-
-
+# feature creation the number of ip address per user
+def feature_creation_ip_address(data):
+    data['number_users_perIP'] = data.groupby('ip_address')['user_id'].transform('count')
+    nan_values = data['number_users_perIP'].isna().sum()
+    text_output = '##################################'
+    text_output += 'Feature Creation'
+    text_output += '##################################\n'
+    text_output += 'Number of users per IP:'
+    text_output += str(nan_values) + '\n'
+    save_to_file('report.txt', text_output)
+    return data
+# feature creation the number of signup_time per device id
+def feature_creation_signup_time(data):
+    data['signup_count_PerDevice'] = data.groupby('device_id')['signup_time'].transform('count')
+    nan_values = data['signup_count_PerDevice'].isna().sum()
+    text_output = '##################################'
+    text_output += 'Feature Creation'
+    text_output += '##################################\n'
+    text_output += 'Number of signups per device:'
+    text_output += str(nan_values) + '\n'
+    save_to_file('report.txt', text_output)
+    return data
+# feature creation between purchase time and signup time
+def feature_creation_time_difference(data):
+    data['time_difference'] = (data['purchase_time'] - data['signup_time']).dt.total_seconds()
+    data['time_difference'] = data['time_difference'].astype(int)
+    nan_values = data['time_difference'].isna().sum()
+    text_output = '##################################'
+    text_output += 'Feature Creation'
+    text_output += '##################################\n'
+    text_output += 'Time Difference:'
+    text_output += str(nan_values) + '\n'
+    save_to_file('report.txt', text_output)
+    return data
+# feature creation extract from signup time week number, day number of month, day number of week, hour(mm and ss) in seconds
+def feature_extraction_signup_time(data):
+    data['signup_week'] = data['signup_time'].dt.isocalendar().week
+    data['signup_day'] = data['signup_time'].dt.day
+    data['signup_day_of_week'] = data['signup_time'].dt.dayofweek
+    data['signup_time_seconds'] = data['signup_time'].dt.hour * 3600 + data['signup_time'].dt.minute * 60 + data['signup_time'].dt.second
+    nan_values = data[['signup_week', 'signup_day', 'signup_day_of_week', 'signup_time_seconds']].isna().sum()
+    text_output = '##################################'
+    text_output += 'Feature Creation'
+    text_output += '##################################\n'
+    text_output += 'Signup TIme:\n'
+    text_output += str(nan_values['signup_week']) + '\n'
+    text_output += str(nan_values['signup_day']) + '\n'
+    text_output += str(nan_values['signup_day_of_week']) + '\n'
+    text_output += str(nan_values['signup_time_seconds']) + '\n'
+    save_to_file('report.txt', text_output)
+    return data
+# feature creation extract from purchase time week number, day number of month, day number of week, hour(mm and ss) in seconds
+def feature_extraction_purchase_time(data):
+    data['purchase_week'] = data['purchase_time'].dt.isocalendar().week
+    data['purchase_day'] = data['purchase_time'].dt.day
+    data['purchase_day_of_week'] = data['purchase_time'].dt.dayofweek
+    data['purchase_time_seconds'] = data['purchase_time'].dt.hour * 3600 + data['purchase_time'].dt.minute * 60 + data['purchase_time'].dt.second
+    nan_values = data[['purchase_week', 'purchase_day', 'purchase_day_of_week', 'purchase_time_seconds']].isna().sum()
+    text_output = '##################################'
+    text_output += 'Feature Creation'
+    text_output += '##################################\n'
+    text_output += 'Purchase Time:\n'
+    text_output += str(nan_values['purchase_week']) + '\n'
+    text_output += str(nan_values['purchase_day']) + '\n'
+    text_output += str(nan_values['purchase_day_of_week']) + '\n'
+    text_output += str(nan_values['purchase_time_seconds']) + '\n'
+    save_to_file('report.txt', text_output)
+    return data
+# feature creation count the number of user per device id
+def feature_creation_user_per_device(data):
+    data['number_user_per_device'] = data.groupby('device_id')['user_id'].transform('count')
+    nan_values = data['number_user_per_device'].isna().sum()
+    text_output = '##################################'
+    text_output += 'Feature Creation'
+    text_output += '##################################\n'
+    text_output += 'User per device:\n'
+    text_output += str(nan_values) + '\n'
+    save_to_file('report.txt', text_output)
+    return data
+# feature creation count the number of user per country
+def feature_creation_user_per_country(data):
+    data['number_user_per_country'] = data.groupby('country')['user_id'].transform('count')
+    nan_values = data['number_user_per_country'].isna().sum()
+    text_output = '##################################'
+    text_output += 'Feature Creation'
+    text_output += '##################################\n'
+    text_output += 'User per country:\n'
+    text_output += str(nan_values) + '\n'
+    save_to_file('report.txt', text_output)
+    return data
 if __name__ == '__main__':
     data = read_data('EFraud_Data_Country.csv')
     #dublicates_values(data)
@@ -74,6 +163,17 @@ if __name__ == '__main__':
     # tukey_method(data, 'purchase_value')
     # tukey_method(data, 'age')
 ############ Feature Engineering ################
+    data = feature_creation_ip_address(data)
+    data = feature_creation_signup_time(data)
+    data = feature_creation_time_difference(data)
+    data = feature_extraction_signup_time(data)
+    data = feature_extraction_purchase_time(data)
+    data = feature_creation_user_per_device(data)
+    data = feature_creation_user_per_country(data)
+    print(data.head())
+
+
+
 
 
 
