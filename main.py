247f0cbfcb4d8688910f1387ec3f8d3f632bf19f
icicala <icicala.ubb@gmail.com>
2024-02-29 00:47:21 +0100

    FRAUD-26 Setup Correlation analysis

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
-
 import numpy as np
 import pandas as pd
-
-
-
+from sklearn.preprocessing import OneHotEncoder
+import category_encoders as ce
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
 # load EFraud_Dataset with country
 # Read data from csv files
 def read_data(filename, delimiter=','):
@@ -156,6 +157,63 @@ def feature_creation_user_per_country(data):
     text_output += str(nan_values) + '\n'
     save_to_file('report.txt', text_output)
     return data
+# log transformation of the purchase value and age in dataset
+def log_transformation(data):
+    data['purchase_value'] = np.log(data['purchase_value'] + 1)
+    data['age'] = np.log(data['age'])
+    return data
+# one hot encoding for source, browser, sex
+def one_hot_encoding(data):
+    categorical_columns = ['source', 'browser', 'sex']
+    encoder = OneHotEncoder(drop='first',sparse_output=False)
+    encoded = encoder.fit_transform(data[categorical_columns])
+    encoded = pd.DataFrame(encoded, columns=encoder.get_feature_names_out(categorical_columns))
+    data = pd.concat([data, encoded], axis=1)
+    return data
+# binary encoding for device id and country
+def binary_encoding(data):
+    categorical_columns = ['device_id', 'country']
+    encoder = ce.BinaryEncoder(cols=categorical_columns, return_df=True)
+    encoded = encoder.fit_transform(data[categorical_columns])
+    data = pd.concat([data, encoded], axis=1)
+    return data
+# drop the columns that are not needed
+def drop_columns(data):
+    columns = ['signup_time', 'purchase_time', 'device_id', 'ip_address', 'country', 'source', 'browser', 'sex', 'user_id']
+    data = data.drop(columns, axis=1)
+    # move class to the last column
+    data = data[[c for c in data if c not in ['class']] + ['class']]
+    return data
+# heatmap for correlation that are higher than 0.4 and lower than -0.4
+def heatmap(data):
+    corr = data.corr()
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+    plt.figure(figsize=(30, 30))
+    sns.heatmap(corr[(corr >= 0.3) | (corr <= -0.3)], annot=True, mask=mask, cmap='coolwarm', linewidths=0.5,
+                square=True)
+    plt.show()
+# feature importance using random forest classifier
+def feature_importance(data):
+    X = data.drop('class', axis=1)
+    y = data['class']
+    model = RandomForestClassifier(n_estimators=500, random_state=1, n_jobs=-1)
+    model.fit(X, y)
+    importance = model.feature_importances_
+    index = np.argsort(importance)[::-1]
+    for i in range(X.shape[1]):
+        print(f'{X.columns[index[i]]}: {importance[index[i]]}')
+    plt.title('Feature Importance using Random Forest Classifier')
+    plt.bar(range(X.shape[1]), importance[index], align='center', color='grey')
+    plt.xticks(range(X.shape[1]), X.columns[index], rotation=90)
+    plt.xlim([-1, X.shape[1]])
+    plt.tight_layout()
+    plt.show()
+# drop feature signup_count_PerDevice, number_user_perIP, signup_week
+def drop_features(data):
+    columns = ['signup_count_PerDevice', 'number_users_perIP', 'signup_week']
+    data = data.drop(columns, axis=1)
+    return data
+
 if __name__ == '__main__':
     data = read_data('EFraud_Data_Country.csv')
     #dublicates_values(data)
@@ -170,7 +228,16 @@ if __name__ == '__main__':
     data = feature_extraction_purchase_time(data)
     data = feature_creation_user_per_device(data)
     data = feature_creation_user_per_country(data)
-    print(data.head())
+    data = log_transformation(data)
+    data = one_hot_encoding(data)
+    data = binary_encoding(data)
+    data = drop_columns(data)
+    #heatmap(data)
+    # feature_importance(data)
+    data = drop_features(data)
+    feature_importance(data)
+    # data.to_csv('EFraud_Data_Country_Processed.csv', index=False)
+
 
 
 
